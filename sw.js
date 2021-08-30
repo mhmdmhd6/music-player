3ccc0e41c100aee9c248213b3c37cdb7cf3a2535
mhmdmhd6 <79286306+mhmdmhd6@users.noreply.github.com>
2021-08-30 16:24:49 +0430

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,10 @@
 // install service worker
 self.addEventListener('install', evnt => {
-    console.log("Service worker was installed");
+    console.log("Service worker was installed!");
+});
+
+
+// activate service worker
+self.addEventListener('activate', evnt => {
+    console.log("Service worker was activated!");
 });
