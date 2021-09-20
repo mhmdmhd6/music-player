64dce5703bcd88a2cbcd3bff84f0caec3372fe2d
mhmdmhd6 <79286306+mhmdmhd6@users.noreply.github.com>
2021-09-20 21:08:23 +0430

    ....

--- javascript/app.js
+++
@@ -1,9 +0,0 @@
-if ("serviceWorker" in navigator) {
-    navigator.serviceWorker.register("sw.js", {
-        scope: "./"
-    }).then(function (reg) {
-        console.log("Service worker registered", reg);
-    })["catch"](function (err) {
-        console.log(err);
-    });
-}
